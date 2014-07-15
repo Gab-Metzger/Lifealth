8487032c117059c2d6be54bcba150a3229c3bbfe
Gabriel METZGER <gab.metzger@gmail.com>
2014-07-15 22:50:27 +0200

    getAccessToken method

--- app/scripts/controllers/patient.js
+++ app/scripts/controllers/patient.js
@@ -1,11 +1,22 @@
 'use strict';
 
 angular.module('lifealthApp')
-  .controller('PatientCtrl', function ($scope, Auth, $location) {
+  .controller('PatientCtrl', function ($scope, Auth, $location, $http, $rootScope) {
     $scope.logout = function() {
       Auth.logout()
         .then(function() {
           $location.path('/');
         });
     };
+
+    $scope.getBPDatas = function() {
+        $http.get('/api/users/'+$rootScope.currentUser.id)
+            .success(function(data) {
+                console.log(data);
+                // TODO something
+            })
+            .error(function(data) {
+                console.log(data);
+        });
+    }
   });
