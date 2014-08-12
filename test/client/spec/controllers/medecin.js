3b745d3676bc679b847b38f1860e0039f458f2ea
Gabriel METZGER <gab.metzger@gmail.com>
2014-08-12 07:36:51 +0200

    test médecin contrôler

--- test/client/spec/controllers/medecin.js
+++ test/client/spec/controllers/medecin.js
@@ -6,17 +6,27 @@ describe('Controller: MedecinCtrl', function () {
   beforeEach(module('lifealthApp'));
 
   var MedecinCtrl,
-    scope;
+    scope,hb,rt;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
     scope = $rootScope.$new();
-    MedecinCtrl = $controller('MedecinCtrl', {
-      $scope: scope
-    });
+    hb = $httpBackend;
+    rt = $rootScope;
+    rt.currentUser = {};
+    rt.currentUser.id = '123';
+    MedecinCtrl = $controller;
   }));
 
   //it('should attach a list of awesomeThings to the scope', function () {
     //expect(scope.awesomeThings.length).toBe(3);
   //});
+  it('should return records', function() {
+    hb.expectGET('/api/doctors/'+rt.currentUser.id+'/records').respond([{'id': 123,'firstName': 'Gabriel', 'lastName': 'METZGER'}]);
+    MedecinCtrl('MedecinCtrl', {
+      $scope: scope
+    });
+    hb.flush();
+    expect(scope.data).toBe(1);
+  });
 });
